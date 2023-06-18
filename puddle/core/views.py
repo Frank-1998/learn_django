ebdd35401fe08511df1fe37bb8e2ec56b0b090fb
unknown <ziang6@ualberta.ca>
2023-06-18 09:12:02 -0600

    modify gitignore so that it ignores *.sqlite3, save informations in sign up form and redirect to login

--- puddle/core/views.py
+++ puddle/core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from item.models import Category, Item
 from .forms import SignupForm
 def index(request):
@@ -14,7 +14,15 @@ def contact(request):
     return render(request, 'core/contact.html')
 
 def Signup(request):
-    form = SignupForm()
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('/login/')
+    else:
+        form = SignupForm()
+    
     return render(request, 'core/signup.html', {
         'form': form
     })
