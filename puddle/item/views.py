e8b90078ca329135e263e9eae8b118e68699ba59
unknown <ziang6@ualberta.ca>
2023-06-23 10:24:23 -0600

    add search function by inputing keyword or selecting filters

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Item
+from .models import Item, Category
+from django.db.models import Q
 from django.contrib.auth.decorators import login_required
 from . forms import NewItemForm, EditItemForm
 # Create your views here.
 
 def items(request):
+    query = request.GET.get('query', '')
+    categories = Category.objects.all()
+    category_id = request.GET.get('category', 0)
     items = Item.objects.filter(is_sold=False)
+    if query:
+        items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
+    if category_id:
+        items = items.filter(category_id=category_id)
     return render(request, 'item/items.html', {
         'items': items,
+        'query': query,
+        'categories': categories,
+        'category_id': int(category_id),
     })
 
 def detail(request, pk):
