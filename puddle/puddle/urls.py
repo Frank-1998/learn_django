904d2412b484063edee9c628def072af207e4d0b
unknown <ziang6@ualberta.ca>
2023-07-01 20:33:33 -0600

    set up urls and templates for conversations

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('', include('core.urls')),
+    path('inbox/', include('conversation.urls')),
     path('items/', include('item.urls')),
     path('dashboard/', include('dashboard.urls')),
     path('admin/', admin.site.urls),
